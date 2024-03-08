f0853d780d7b504ca78085dbfbb55a3c18cc5241
Artyom Sokolov <15012003artem@mail.ru>
2024-03-08 23:16:41 +0500

    Refactor GitHubClientTest

--- scrapper/src/test/java/edu/java/scrapper/client/GitHubClientTest.java
+++ scrapper/src/test/java/edu/java/scrapper/client/GitHubClientTest.java
@@ -2,44 +2,39 @@ package edu.java.scrapper.client;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
+import com.github.tomakehurst.wiremock.junit5.WireMockRuntimeInfo;
 import com.github.tomakehurst.wiremock.junit5.WireMockTest;
 import edu.java.client.GitHubClient;
 import edu.java.dto.RepositoryInformation;
 import java.time.OffsetDateTime;
 import java.util.Map;
 import java.util.Optional;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.http.HttpHeaders;
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
 import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlPathMatching;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @WireMockTest(httpPort = 8080)
 public class GitHubClientTest {
     private static final GitHubClient GITHUB_CLIENT = new GitHubClient("http://localhost:8080", new HttpHeaders());
-    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
-    private static final String OWNER_NAME = "alex";
-    private static final String REPOSITORY_NAME = "java-work";
+    private static final String OWNER_NAME = "test-user";
+    private static final String REPOSITORY_NAME = "test-repos";
     private static final String UPDATE_TIME = "2024-02-18T18:26:28Z";
 
     @Test
-    public void testGetInformationAboutRepository() throws JsonProcessingException {
-        Map<String, String> firstParameters = Map.of("name", REPOSITORY_NAME, "pushed_at", UPDATE_TIME);
-        stubFor(get(urlPathMatching(String.join("/", "/repos", OWNER_NAME, REPOSITORY_NAME)))
-            .willReturn(aResponse()
-                .withStatus(200)
-                .withHeader("Content-Type", "application/json")
-                .withBody(OBJECT_MAPPER.writeValueAsString(firstParameters))));
-
-        stubFor(get(urlPathMatching(String.join("/", "/repos", OWNER_NAME, REPOSITORY_NAME, "activity")))
-            .willReturn(aResponse()
-                .withStatus(200)
-                .withHeader("Content-Type", "application/json")
-                .withBody("[{\"activity_type\":\"push\"}]")));
+    public void testGetInformationAboutRepository(WireMockRuntimeInfo wmRuntimeInfo) {
+        WireMock wireMock = wmRuntimeInfo.getWireMock();
+        wireMock.loadMappingsFrom("src/test/resources/github-wiremock-templates/");
 
         RepositoryInformation expected =
             new RepositoryInformation(
@@ -48,6 +43,7 @@ public class GitHubClientTest {
                 RepositoryInformation.GithubActivity.PUSH
             );
         RepositoryInformation actual = GITHUB_CLIENT.getRepositoryInformation(OWNER_NAME, REPOSITORY_NAME).get();
+
         assertEquals(expected, actual);
     }
 
@@ -58,25 +54,25 @@ public class GitHubClientTest {
             .willReturn(aResponse().withStatus(404)));
         stubFor(get(urlPathMatching("/repos/some-user/unknown-rep/activity"))
             .willReturn(aResponse().withStatus(404)));
+
         Optional<RepositoryInformation> actual = GITHUB_CLIENT.getRepositoryInformation(OWNER_NAME, REPOSITORY_NAME);
+
         assertTrue(actual.isEmpty());
     }
 
     @Test
     @DisplayName("Exception will not be triggered when unknown type of activity is received")
-    public void testProcessUnknownTypeOfRepositoryActivity() throws JsonProcessingException {
-        Map<String, String> firstParameters = Map.of("name", REPOSITORY_NAME, "pushed_at", UPDATE_TIME);
-        stubFor(get(urlPathMatching(String.join("/", "/repos", OWNER_NAME, REPOSITORY_NAME)))
-            .willReturn(aResponse()
-                .withStatus(200)
-                .withHeader("Content-Type", "application/json")
-                .withBody(OBJECT_MAPPER.writeValueAsString(firstParameters))));
+    public void testProcessUnknownTypeOfRepositoryActivity(WireMockRuntimeInfo wmRuntimeInfo) {
+        WireMock wireMock = wmRuntimeInfo.getWireMock();
+        wireMock.loadMappingsFrom("src/test/resources/github-wiremock-templates/");
         stubFor(get(urlPathMatching(String.join("/", "/repos", OWNER_NAME, REPOSITORY_NAME, "activity")))
             .willReturn(aResponse()
                 .withStatus(200)
                 .withHeader("Content-Type", "application/json")
                 .withBody("[{\"activity_type\":\"unknown_activity\"}]")));
+
         RepositoryInformation actual = GITHUB_CLIENT.getRepositoryInformation(OWNER_NAME, REPOSITORY_NAME).get();
+
         assertEquals(RepositoryInformation.GithubActivity.UNKNOWN, actual.getLastActivity());
     }
 }
