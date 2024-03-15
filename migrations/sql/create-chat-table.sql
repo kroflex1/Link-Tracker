6eb34515600950155c0f844bb8deec64820c61b4
Artyom Sokolov <15012003artem@mail.ru>
2024-03-15 22:57:53 +0500

    Refactor sql

--- migrations/sql/create-chat-table.sql
+++ migrations/sql/create-chat-table.sql
@@ -4,7 +4,7 @@
 CREATE TABLE chats
 (
     chat_id    BIGINT                   NOT NULL,
-    created_at timestamp with time zone not null NOT NULL,
+    created_at timestamp WITH TIME ZONE NOT NULL,
 
     PRIMARY KEY (chat_id)
 )
--- migrations/sql/create-link-table.sql
+++ migrations/sql/create-link-table.sql
@@ -3,8 +3,8 @@
 
 CREATE TABLE links
 (
-    link             VARCHAR(300) NOT NULL,
-    created_time     TIMESTAMP    NOT NULL,
-    last_time_update TIMESTAMP    NOT NULL,
+    link             VARCHAR(300)             NOT NULL,
+    created_at       TIMESTAMP WITH TIME ZONE NOT NULL,
+    last_time_update TIMESTAMP WITH TIME ZONE NOT NULL,
     PRIMARY KEY (link)
 )
