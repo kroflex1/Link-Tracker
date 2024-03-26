a7338871706b416c75f9bc3b35a120fc67cf5ea2
Artyom Sokolov <15012003artem@mail.ru>
2024-03-26 23:18:24 +0500

    Refactor code style

--- dto/src/main/java/edu/java/request/LinkUpdateRequest.java
+++ dto/src/main/java/edu/java/request/LinkUpdateRequest.java
@@ -1,8 +1,6 @@
 package edu.java.request;
 
 import java.util.List;
-import lombok.Value;
-import wiremock.com.fasterxml.jackson.annotation.JsonProperty;
 
 public record LinkUpdateRequest(String url,  String description,
                                  List<Long> tgChatsId) {
